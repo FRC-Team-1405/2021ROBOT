e3fdf197293bdcd91f056a4236b398f93f12b3be
Finney Student <none>
2021-06-04 22:36:52 -0400

    update diatance tables

--- src/main/java/frc/robot/lib/DistanceToAngle.java
+++ src/main/java/frc/robot/lib/DistanceToAngle.java
@@ -2,14 +2,14 @@ package frc.robot.lib;
 
 public class DistanceToAngle {
     private static Interpolate table = new Interpolate( new Interpolate.Point[] {
-        new Interpolate.Point(75.8, 0),
-        new Interpolate.Point(153.0, 85000),
-        new Interpolate.Point(261.0, 155000), 
-        new Interpolate.Point(411.1, 200000), 
-        new Interpolate.Point(562.7, 225000), 
-        new Interpolate.Point(722.3, 250000)
-      });
-  
+      new Interpolate.Point(75.8, 0),
+      new Interpolate.Point(153.0, 10000),
+      new Interpolate.Point(261.0, 15000), 
+      new Interpolate.Point(411.1, 20000), 
+      new Interpolate.Point(562.7, 32500), 
+      new Interpolate.Point(722.3, 34000)
+    });
+        
       /**
        * Convert a distance in centimeters to a power setting.
        * @param distance in centimeters
--- src/main/java/frc/robot/lib/DistanceToPower.java
+++ src/main/java/frc/robot/lib/DistanceToPower.java
@@ -3,13 +3,14 @@ package frc.robot.lib;
 /** Add your docs here. */
 public class DistanceToPower { 
   private static Interpolate table = new Interpolate( new Interpolate.Point[] {
-    new Interpolate.Point(75.8, 12000),
-    new Interpolate.Point(153.0, 12000),
-    new Interpolate.Point(261.0, 12000), 
-    new Interpolate.Point(411.1, 12000), 
-    new Interpolate.Point(562.7, 12000), 
-    new Interpolate.Point(722.3, 14000)
+    new Interpolate.Point(203.0,  8000),
+    new Interpolate.Point(303.0,  9000),
+    new Interpolate.Point(402.0,  9500), 
+    new Interpolate.Point(497.0, 10000), 
+    new Interpolate.Point(589.0, 11000), 
+    new Interpolate.Point(710.0, 12000)
   });
+
     /**
      * Convert a distance in centimeters to a power setting.
      * @param distance in centimeters
